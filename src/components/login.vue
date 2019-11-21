f1a2c44257e749022ccf28216ba0eed40c7e2768
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2019-11-21 18:41:07 +0800

    美化登录摁钮

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,116 +1,133 @@
 <template>
-	<div id="login">
-    <div id="container">
-      <div id="logobox">
-        <img src=""  >
-			</div>
-      <div id="login_item">
-				<span class="login span">
-					<div class="align_tool_1">
-						帐号:<input type="text" class="account" name="" id="" value="" />
-					</div>
-				</span>
-				<span class="login span">
-					<div class="align_tool_2">
-						密码:<input type="password" class="password" name="" />
-					</div>
-				</span>
-				<span class="span">
-          <div class="align_tool_3">
-            <a href="#">忘记密码</a>
-          </div>
-          </span>
+	<div id="container">
+		<div id="login_item">
+			<span class="login span">
+				<div class="align_tool_1">
+					帐号<span class="input_account"><input type="text" class="account" name="" id="" value="" /></span>
+				</div>
+			</span>
+			<span class="login span">
+				<div class="align_tool_2">
+					密码<span class="input_password"><input type="password" class="password" name="" /></span>
+				</div>
+			</span>
+			<span class="options">
+				<div class="align_tool_3">
+					<a href="#">注册</a>
+					<a href="#">忘记密码</a>
+				</div>
+			</span>
 			<div id="button_container">
 				<span><button type="button">登录</button></span>
 			</div>
-				<div class="registry">还没有帐号？赶紧<a href="#">注册</a>一个吧！</div>
-			</div>
-    </div>
+		</div>
   </div>
 </template>
+
 <script>
   export default {
     name: 'login'
   }
 </script>
+			
 		<style>
-			img {
-				width: 1.06rem;
-				height: 1.03rem;
-			}
-			#logobox {
-				width: 100%;
-				height: 1.03rem;
-				display: flex;
-				justify-content: center;
-				}
 			#login_item {
 				display: flex;
-				width: 3rem;
-				height: 2.0rem;
+				
 				flex-wrap: wrap;
-				align-content: flex-end;
+				align-content: space-around;
 				justify-content: center;
+				
+				margin-top: .6rem;	
+				width: 3rem;
+				height: 2rem;
 			}
+			
 			#container {
 				display: flex;
+				
 				align-content: center;
 				justify-content: center;
-				/*flex-direction: column;*/
 				flex-wrap: wrap;
+				
 				margin-top: 1.03rem;
 			}
-			.span {
+			
+			.span {				
 				width: 3rem;
-				/*width: 300px;*/
-				height: 0.3rem;
+				height: .5rem;
 			}
-			.account, .password {
-				padding: 0.03rem;
-				width: 1.8rem;
-				font-size: 0.18rem;
+			
+			.account, .password {				
+				width: 1.6rem;
+				font-size: .18rem;
 				border: none;
+				padding: .03rem;
 				outline: 0;
-				border-bottom:0.01rem black solid ;
 			}
-
+			
 			a {
 				color: aqua;
 				text-decoration: none;
 			}
+			
 			a:hover {
 				color: red;
 			}
+			
+			.input_account, .input_password {
+				margin-left: 0.05rem;
+			}
+			
 			.align_tool_1, .align_tool_2 {
-				margin-left: 0.1rem;
+				margin-left: .3rem;
+				border: black 1px solid;
+				border-radius: 0.10rem;				
+				padding: .08rem;
+				width: 2.2rem;
+				height: .25rem;
 			}
 			
 			.align_tool_3 {
-				margin-left: 2.2rem;
-				font-size: 0.15rem;
-			}
-			#button_container {
-				/*margin-left: 10rem;*/
-				height: 0.44rem;
-				margin-left: 0.9rem;
-			}
-			button {
-				background-color: white;
-				border: solid gray 0.01rem;
-				border-radius: 20% 20%;
-				height: 0.44rem;
-				width: 0.9rem;
-				font-size: 0.2rem;
+				width: 75%;
+				margin-left: 0.4rem;
+				margin-top: rem;
+				font-size: 0.12rem;
+				display: flex;
+				justify-content: space-between;
+				margin-top: -0.08rem;
 			}
 			
+			.options {
+				width: 3rem;
+				/*width: 300px;*/
+				height: 0.2rem;
+			}
+			
+			#button_container {
+				margin-left: 0.02rem;
+				height: 0.44rem;
+			}
+			
+			button {				
+				margin-top: -0.08rem;
+				border: solid gray 0.01rem;
+				border-radius: 0.25rem;
+				height: 0.52rem;
+				width: 1rem;
+				
+				font-size: 0.15rem;
+				
+				background-color: white;
+			}
+			
+			button:active {
+				background-color: gray;
+				box-shadow: 0 3px grey;
+				transform: translateY(2px)
+			}
+
 			.login{
-				font-weight: bold;
-				font-size: 0.18rem;
+				font-size: 0.12rem;
 			}
-			.registry {
-				width: 0.7rem;
-				height: 0.4rem;
-				font-size: 0.1rem;
-				padding-left:0.2rem;
-			}
-		</style>
+	</style>
