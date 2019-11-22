15f8e98b2ab6a334260e7aaf19e984b60cb72b74
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2019-11-22 21:26:18 +0800

    缩进问题

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,15 @@
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/register.vue
+++ src/components/register.vue
@@ -1,180 +1,198 @@
 <template>
   <div id="body">
-	<div id="container">
-		<div class="box">
-			<input type="text" class="input" @input="checkem(email)" v-model="email" placeholder="您的邮箱地址"/>
-		</div>
-		<p :class="{active:em}">邮箱地址不能为空</p>
-		<div class="box">
-			<input type="text" class="input" @input="checksc(account)" v-model="account" placeholder="您的学号"/>
-		</div>
-		<p :class="{active:sc}">学号不为空</p>
-		<div class="box">
-			<input type="password" class="input" @input="checkpw1(password)" v-model="password" placeholder="您的密码"/>
-		</div>
-		<p :class="{active:pw1}">密码不为空</p>
-		<div class="box">
-			<input type="password" class="input" @input="checkpassword(password,passwords)" v-model="passwords" placeholder="确认密码"/>
-		</div>
-		<p :class="{active: checkpw2}">前后密码需一致！</p>    
-    <div class=button v-if="account===''||email===''||password===''||passwords===''||checkpw2">
-      <button class="btn2">注册</button>
+    <div id="container">
+      <div class="box">
+        <input
+          type="text"
+          class="input"
+          @input="checkem(email)"
+          v-model="email"
+          placeholder="您的邮箱地址"
+        />
+      </div>
+      <p :class="{active:em}">邮箱地址不能为空</p>
+      <div class="box">
+        <input
+          type="text"
+          class="input"
+          @input="checksc(account)"
+          v-model="account"
+          placeholder="您的学号"
+        />
+      </div>
+      <p :class="{active:sc}">学号不为空</p>
+      <div class="box">
+        <input
+          type="password"
+          class="input"
+          @input="checkpw1(password)"
+          v-model="password"
+          placeholder="您的密码"
+        />
+      </div>
+      <p :class="{active:pw1}">密码不为空</p>
+      <div class="box">
+        <input
+          type="password"
+          class="input"
+          @input="checkpassword(password,passwords)"
+          v-model="passwords"
+          placeholder="确认密码"
+        />
+      </div>
+      <p :class="{active: checkpw2}">前后密码需一致！</p>
+      <div class="button" v-if="account===''||email===''||password===''||passwords===''||checkpw2">
+        <button class="btn2">注册</button>
+      </div>
+      <div class="button" v-else>
+        <button class="btn1">注册</button>
+      </div>
+      <div class="return">
+        <a href="/">返回登录</a>
+      </div>
     </div>
-		<div class="button" v-else>
-			<button class="btn1">注册</button>
-		</div>
-		<div class="return"><a href="/">返回登录</a></div>
-	</div>
   </div>
 </template>
 	
 <script>
-  export default {
-    name: 'register',
-    data() {
-      return {
-        account: '',
-        email: '',
-        password: '',
-				passwords: '',
-				checkpw2: false,
-				em: false,
-				sc: false,
-				pw1: false
-      }
-		},
-		methods: {
-			checkpassword: function(password,passwords){
-				if (password != passwords)
-				this.checkpw2 = true;
-				if (password === passwords)
-				this.checkpw2 = false;
-			},
-			checkpw1: function(password){
-				if (password === '')
-				this.pw1 = true;
-				if (password != '')
-				this.pw1 = false;
-			},
-			checksc: function(account){
-				if (account === '')
-				this.sc = true;
-				if (account != '')
-				this.sc = false;
-			},
-			checkem: function(email){
-				if (email === '')
-				this.em = true;
-				if (email != '')
-				this.em = false;
-			}
-			
-		}
+export default {
+  name: "register",
+  data() {
+    return {
+      account: "",
+      email: "",
+      password: "",
+      passwords: "",
+      checkpw2: false,
+      em: false,
+      sc: false,
+      pw1: false
+    };
+  },
+  methods: {
+    checkpassword: function(password, passwords) {
+      if (password != passwords) this.checkpw2 = true;
+      if (password === passwords) this.checkpw2 = false;
+    },
+    checkpw1: function(password) {
+      if (password === "") this.pw1 = true;
+      if (password != "") this.pw1 = false;
+    },
+    checksc: function(account) {
+      if (account === "") this.sc = true;
+      if (account != "") this.sc = false;
+    },
+    checkem: function(email) {
+      if (email === "") this.em = true;
+      if (email != "") this.em = false;
+    }
   }
+};
 </script>
 
 <style>
-	#body {
-    display: flex;
-    justify-content: center;
-    align-content: center;
-		
-  }
-  
-  #container {
-		display: flex;
-		justify-content: center;
-		align-content: center;
-		flex-wrap: wrap;
-				
-		margin-top: -1.2rem;
-		width: 3.75rem;
-    height: 7.67rem;
-		
-		background-image:url('..\img\background.png');
-		background-size: 3.75rem 7.67rem;
-	}
-			
-	.box {
-		display: flex;
-		justify-content: center;
-				
-		margin: rem;
-		padding-top: .07rem;
-		border: #000000 1px solid;
-		border-radius: .17rem;
-		width: 2.2rem;
-		height: .32rem;
-				
-		font-size: .15rem;
+#body {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
 
-		background-color: white;
-	}
+#container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
 
-		.return {
-		display: flex;
-		justify-content: center;
-				
-		margin: rem;
-		padding-top: .2rem;
-		border-radius: .17rem;
-		width: 2.2rem;
-		height: .32rem;
-				
-		font-size: .15rem;
-	}
-			
-  .button {
-    display: flex;
-		justify-content: center;
-				
-		margin: .04rem;
-		padding-top: .07rem;
-		width: 2.2rem;
-		height: .32rem;
-	}
+  margin-top: -1.2rem;
+  width: 3.75rem;
+  height: 7.67rem;
 
-	.btn1, .btn2 {
-		border-radius: 2rem;
-		width: 1rem;
-		height: .5rem;
+  background-image: url("..\img\background.png");
+  background-size: 3.75rem 7.67rem;
+}
 
-		font-size: .18rem;}
+.box {
+  display: flex;
+  justify-content: center;
 
-	.btn1	{
-		color: white;
-		background: saddlebrown;
-	}
-			
-	.input {
-		border: none;
-		outline: none;
-		width: 1.7rem;
-		height: .22rem;
-				
-		font-size: .15rem;
-	}
+  margin: rem;
+  padding-top: 0.07rem;
+  border: #000000 1px solid;
+  border-radius: 0.17rem;
+  width: 2.2rem;
+  height: 0.32rem;
 
-	p {
-		line-height: .12rem;
-		margin: .01rem;
-		width: 3rem;
+  font-size: 0.15rem;
 
-		font-size: .14rem;
-		color: red; 
-		
-		visibility: hidden;
-	}
-	.active {
-		visibility: visible;
-	}
+  background-color: white;
+}
 
-	a {
-	color: aqua;
-	text-decoration: none;
-	}
+.return {
+  display: flex;
+  justify-content: center;
 
-	a:hover {
-		color: red;
-	}		
+  margin: rem;
+  padding-top: 0.2rem;
+  border-radius: 0.17rem;
+  width: 2.2rem;
+  height: 0.32rem;
+
+  font-size: 0.15rem;
+}
+
+.button {
+  display: flex;
+  justify-content: center;
+
+  margin: 0.04rem;
+  padding-top: 0.07rem;
+  width: 2.2rem;
+  height: 0.32rem;
+}
+
+.btn1,
+.btn2 {
+  border-radius: 2rem;
+  width: 1rem;
+  height: 0.5rem;
+
+  font-size: 0.18rem;
+}
+
+.btn1 {
+  color: white;
+  background: saddlebrown;
+}
+
+.input {
+  border: none;
+  outline: none;
+  width: 1.7rem;
+  height: 0.22rem;
+
+  font-size: 0.15rem;
+}
+
+p {
+  line-height: 0.12rem;
+  margin: 0.01rem;
+  width: 3rem;
+
+  font-size: 0.14rem;
+  color: red;
+
+  visibility: hidden;
+}
+.active {
+  visibility: visible;
+}
+
+a {
+  color: aqua;
+  text-decoration: none;
+}
+
+a:hover {
+  color: red;
+}
 </style>
